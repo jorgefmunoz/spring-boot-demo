db775268159a0ddaf8b70540a30177b20c637af2
Jorge Munoz <jorge.munoz@qualicom.com>
2020-02-03 11:23:35 -0500

    Dev Tools and Actuator support added

--- src/main/java/com/qcom/springboot/demo/myapp/rest/FunRestController.java
+++ src/main/java/com/qcom/springboot/demo/myapp/rest/FunRestController.java
@@ -8,10 +8,20 @@ import java.time.LocalDateTime;
 @RestController
 public class FunRestController {
 
-    // expose "." that return "Hello World"
+    // expose "/" that return "Hello World"
 
     @GetMapping
     public String sayHello() {
         return "Hello World! Time on sever is " + LocalDateTime.now();
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+
+    @GetMapping("/fortune")
+    public  String getDailyFortune() {
+        return "Today is your lucky day";
     }
 }
